1c2a80b970c7c40953f0d3aa0fad700c27738577
JekaLion <djoni-01@mail.ru>
2019-01-12 13:54:17 +0600

    init

---
+++ Services.Abstract/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Abstract
+{
+    public interface IEmailService
+    {
+        Task SendPlainEmail(string to, string title, string text);
+    }
+}
---
+++ Services/GmailService.cs
@@ -0,0 +1,16 @@
+﻿using Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class GmailService : IEmailService
+    {
+        public Task SendPlainEmail(string to, string title, string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
